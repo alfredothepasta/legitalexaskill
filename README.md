10ea7845cc6508278ae872eff52948c4766544d6
davidhorton <davidhorton@users.noreply.github.com>
2023-05-08 23:24:36 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+## CS 3230
+
+This project is pretty dumb currently. Just a simple gradle based jar project. All future assignments
+will be built on top of this starting point.
---
+++ src/main/java/com/weber/cs3230/ChatBotMain.java
@@ -0,0 +1,8 @@
+package com.weber.cs3230;
+
+public class ChatBotMain {
+
+    public static void main(String[] args) {
+        System.out.println("assignment 1 base up and running");
+    }
+}

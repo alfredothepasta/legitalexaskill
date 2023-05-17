2668f7c6a584ac47499b505d061195e3287500aa
Fig Newton Reed <robertalexreed@gmail.com>
2023-05-16 19:32:17 -0600

    because I'm dumb

--- src/main/java/com/weber/cs3230/ChatBotLoop.java
+++ src/main/java/com/weber/cs3230/ChatBotLoop.java
@@ -1,5 +1,18 @@
 package com.weber.cs3230;
 
+import java.util.Scanner;
+
+// I had to think up another class, so I just slapped the loop logic in here. I feel it's pointless to not just put this in the main but I'm following the assignment.
 public class ChatBotLoop {
-    
+    public static void programLoop(){
+        Scanner input = new Scanner(System.in);
+        String question = "";
+
+        // while the question doesn't
+        while(!question.equals("exit")){
+            question = input.nextLine();
+            question.toLowerCase();
+            System.out.println(QuestionLogic.answerQuestion(question));
+        }
+    }
 }

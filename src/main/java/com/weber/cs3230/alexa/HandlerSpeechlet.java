099fcc3ec3bec540435b14ca0b3ff8ab4b453f26
Fig Newton Reed <robertalexreed@gmail.com>
2023-06-08 20:55:34 -0600

    added the things to the git

--- src/main/java/com/weber/cs3230/alexa/HandlerSpeechlet.java
+++ src/main/java/com/weber/cs3230/alexa/HandlerSpeechlet.java
@@ -1,2 +1,28 @@
-package com.weber.cs3230.alexa;public class HandlerSpeechlet {
+package com.weber.cs3230.alexa;
+
+import com.amazon.speech.json.SpeechletRequestEnvelope;
+import com.amazon.speech.speechlet.*;
+import org.springframework.stereotype.Component;
+
+@Component
+public class HandlerSpeechlet implements SpeechletV2 {
+    @Override
+    public void onSessionStarted(SpeechletRequestEnvelope<SessionStartedRequest> requestEnvelope) {
+
+    }
+
+    @Override
+    public SpeechletResponse onLaunch(SpeechletRequestEnvelope<LaunchRequest> requestEnvelope) {
+        return null;
+    }
+
+    @Override
+    public SpeechletResponse onIntent(SpeechletRequestEnvelope<IntentRequest> requestEnvelope) {
+        return null;
+    }
+
+    @Override
+    public void onSessionEnded(SpeechletRequestEnvelope<SessionEndedRequest> requestEnvelope) {
+
+    }
 }

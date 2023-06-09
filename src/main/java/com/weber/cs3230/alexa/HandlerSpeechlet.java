041326289b2fd6e5091db825b4db7707becd2e72
Fig Newton Reed <robertalexreed@gmail.com>
2023-06-08 21:09:09 -0600

    just so I can come back after checking out 4

--- src/main/java/com/weber/cs3230/alexa/HandlerSpeechlet.java
+++ src/main/java/com/weber/cs3230/alexa/HandlerSpeechlet.java
@@ -2,10 +2,20 @@ package com.weber.cs3230.alexa;
 
 import com.amazon.speech.json.SpeechletRequestEnvelope;
 import com.amazon.speech.speechlet.*;
+import com.weber.cs3230.AlexaIntentHandler;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class HandlerSpeechlet implements SpeechletV2 {
+
+    AlexaIntentHandler handler;
+
+    @Autowired
+    public HandlerSpeechlet(AlexaIntentHandler handler) {
+        this.handler = handler;
+    }
+
     @Override
     public void onSessionStarted(SpeechletRequestEnvelope<SessionStartedRequest> requestEnvelope) {
 

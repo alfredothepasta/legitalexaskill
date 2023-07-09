11ef782b710b0d02a072681b2f99353ba53e5cad
alfredothepasta <robertalexreed@gmail.com>
2023-07-09 17:37:10 -0600

    assignment over

--- src/main/java/com/weber/cs3230/adminapp/AlexaAdminApp.java
+++ src/main/java/com/weber/cs3230/adminapp/AlexaAdminApp.java
@@ -41,8 +41,16 @@ public class AlexaAdminApp {
     private JComponent createTablePanel() {
         String[] columnNames = {"Intent", "Date Added"};
         Object[][] data = {
-                {"Susan", 5000},
-                {"Robert", 7000}
+                {"largest_whale_species", "7-9-2023"},
+                {"whale_communication", "7-9-2023"},
+                {"known_for_displays", "7-9-2023"},
+                {"blue_whale_diet", "7-9-2023"},
+                {"toothed_vs_baleen", "7-9-2023"},
+                {"humpback_song_length", "7-9-2023"},
+                {"purpose_of_blowhole", "7-9-2023"},
+                {"how_they_migrate", "7-9-2023"},
+                {"average_whale_lifespan", "7-9-2023"},
+                {"how_whales_use_echolocation", "7-9-2023"}
         };
         DefaultTableModel model = new DefaultTableModel(data, columnNames);
         JTable table = new JTable(model);
@@ -53,9 +61,28 @@ public class AlexaAdminApp {
     }
 
 
-    private JButton createButtonPanel (){
-        JButton button = new JButton("text for the button");
+    private JPanel createButtonPanel (){
+        JPanel buttonPanel = new JPanel(new GridLayout(1, 3));
+        buttonPanel.add(addNewRow());
+        buttonPanel.add(deleteRow());
+        buttonPanel.add(editRow());
+        return buttonPanel;
+
+    }
+
+    private JButton addNewRow(){
+        JButton button = new JButton("Add Row");
         return  button;
+    }
+
+    private JButton deleteRow(){
+        JButton button = new JButton("Delete Row");
+        return  button;
+    }
+
+    private JButton editRow(){
+        JButton button = new JButton("Edit Row");
+        return button;
     }
 
 
